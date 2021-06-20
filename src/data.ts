fa77e23960b56bb4915c365b06c7851ac5f3bfef
Paweł Szynal <pawel@szynal.info>
2021-06-20 19:18:38 +0200

    add clarin picure

--- src/data.ts
+++ src/data.ts
@@ -1,13 +1,13 @@
 import { Place } from "./store/models";
 export const data: Place[] = [
   {
-    title: "Eiffel Tower",
+    title: "Clarin APi",
     description:
       "The Eiffel Tower (/ˈaɪfəl/ EYE-fəl; French: tour Eiffel [tuʁ‿ɛfɛl]) is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower.",
     picture:
-      "https://t2.gstatic.com/images?q=tbn:ANd9GcQ8iFga8aDttbI5iBkKgDmqOSV7LdcsVkfDHezlLYWrGDT7OajbZJh4qS1kiNbN1kkUHmz4SWPOS98AC5HubKYyKw",
+      "./public/Logo-eps-long-CLARIN-sub.png",
     position: [48.858222, 2.2945],
-    seeMoreLink: "https://en.wikipedia.org/wiki/Eiffel_Tower",
+    seeMoreLink: "https://ws.clarin-pl.eu/spacy.shtml",
   },
 
 ];
