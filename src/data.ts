a803b10c650d8d2e0db1a0eaaa30881005f1328a
Paweł Szynal <pawel@szynal.info>
2021-06-20 19:20:53 +0200

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -5,7 +5,7 @@ export const data: Place[] = [
     description:
       "The Eiffel Tower (/ˈaɪfəl/ EYE-fəl; French: tour Eiffel [tuʁ‿ɛfɛl]) is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower.",
     picture:
-      "./public/Logo-eps-long-CLARIN-sub.png",
+      "http://clarin-pl.eu/wp-content/uploads/2020/11/Logo-eps-long-CLARIN-sub.png",
     position: [48.858222, 2.2945],
     seeMoreLink: "https://ws.clarin-pl.eu/spacy.shtml",
   },
